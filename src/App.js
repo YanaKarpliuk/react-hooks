9ab59620cfe815031069edd467fd7e31b304e2f1
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-09-18 11:45:22 +0300

    added styles for mobile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import UseReducerHook from './pages/UseReducerHook'
 import MainPage from "./pages/MainPage";
 import UseMemoHook from './pages/UseMemoHook'
+import UseState from "./pages/UseState";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<MainPage />} />
       <Route path="/use-reducer-hook" element={<UseReducerHook />} />
       <Route path="/use-memo-hook" element={<UseMemoHook />} />
+      <Route path="/use-state-hook" element={<UseState />} />
     </Routes>
   );
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,25 @@
-import { NavLink, Link } from "react-router-dom"
-import '../css/mainPage.css'
+import { NavLink, Link } from "react-router-dom";
+import "../css/mainPage.css";
+import "../css/common.css";
 
 export default function MainPage() {
   return (
     <div>
-      <ul className="main-list">
-        <li><NavLink to="/use-reducer-hook" className="main-list-item">useReducer</NavLink></li>
-        <li><NavLink to="/use-memo-hook" className="main-list-item">useMemo</NavLink></li>
-      </ul>
+      <h2 className="section-title">Learning React Hooks</h2>
+      <div className="section-container">
+        <ul className="main-list">
+          <li>
+            <NavLink to="/use-reducer-hook" className="main-list-item">
+              useReducer
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/use-memo-hook" className="main-list-item">
+              useMemo
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
