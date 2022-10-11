93d07a5b017355e0d7523e5e354b444f24d5377a
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-10-12 00:58:45 +0300

    hashrouter added

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/common.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 // UserProvide is for useContext section
 import { UserProvider } from "./pages/UseContextHookPartials/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <UserProvider>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </UserProvider>
 );
